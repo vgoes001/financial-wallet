14b51b64e46f7c4a5cad877c9aa19956d9e1f5e5
Vitor Goes <vgoes001@gmail.com>
2025-04-08 20:05:53 -0300

    feat(transfer): implement Transfer entity and status value object

---
+++ src/modules/transfer/entities/transfer-status.vo.ts
@@ -0,0 +1,38 @@
+
+export enum TransferStatusEnum {
+  COMPLETED = 'completed',
+  FAILED = 'failed',
+  CANCELLED = 'cancelled',
+  REFUNDED = 'refunded',
+  IN_PROGRESS = 'in_progress',
+}
+
+export class TransferStatusVO{
+  public readonly value: TransferStatusEnum;
+
+  constructor(value: TransferStatusEnum) {
+    this.value = value;
+  }
+
+  static createCompleted(): TransferStatusVO {
+    return new TransferStatusVO(TransferStatusEnum.COMPLETED);
+  }
+
+  static createFailed(): TransferStatusVO {
+    return new TransferStatusVO(TransferStatusEnum.FAILED);
+  }
+
+  static createCancelled(): TransferStatusVO {
+    return new TransferStatusVO(TransferStatusEnum.CANCELLED);
+  }
+
+  static createRefunded(): TransferStatusVO {
+    return new TransferStatusVO(TransferStatusEnum.REFUNDED);
+  }
+
+  static createInProgress(): TransferStatusVO {
+    return new TransferStatusVO(TransferStatusEnum.IN_PROGRESS);
+  }
+
+
+}
---
+++ src/modules/transfer/entities/transfer.entity.ts
@@ -0,0 +1,52 @@
+import { randomUUID } from 'node:crypto';
+import { TransferStatusVO } from './transfer-status.vo';
+
+
+export type TransferConstructorProps = {
+    id?: string;
+    senderId: string;
+    receiverId: string;
+    status?: TransferStatusVO;
+    amount: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+}
+
+export class Transfer {
+  id: string;
+
+  senderId: string;
+
+  receiverId: string;
+
+  amount: number;
+
+  status: TransferStatusVO;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+
+  deletedAt?: Date;
+
+  constructor({
+    id,
+    senderId,
+    receiverId,
+    amount,
+    status,
+    createdAt,
+    updatedAt,
+    deletedAt,
+  }:TransferConstructorProps ) {
+    this.id = id ?? randomUUID();
+    this.senderId = senderId;
+    this.receiverId = receiverId;
+    this.status = status ?? TransferStatusVO.createInProgress();
+    this.amount = amount;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+}
