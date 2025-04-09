3dc045c5694347e5235bf42f7993df5dc980def5
Vitor Goes <vgoes001@gmail.com>
2025-04-08 22:53:32 -0300

    refactor(transfer): add email validation to CreateTransferDto

--- src/modules/transfer/controller/dtos/create-transfer.dto.ts
+++ src/modules/transfer/controller/dtos/create-transfer.dto.ts
@@ -1,8 +1,15 @@
-import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateTransferDto {
   @IsString()
   @IsNotEmpty()
+  @IsEmail()
   @MaxLength(50)
   readonly receiverKey: string;
 
