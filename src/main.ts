1d479db4cd81564333f2d605cbc38032aba71c1c
Vitor Goes <vgoes001@gmail.com>
2025-04-09 22:17:57 -0300

    feat(filters): add InsufficientBalance filter to global filters in main application

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { generateDocs } from './docs';
 import { TransferToSelf } from './modules/shared/filters/transfer-to-self.filter';
 import { EntityValidationFilter } from './modules/shared/filters/entity-validation.filter';
+import { InsufficientBalance } from './modules/shared/filters/insufficient-balance.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new TransferToSelf(), new EntityValidationFilter());
+  app.useGlobalFilters(
+    new TransferToSelf(),
+    new EntityValidationFilter(),
+    new InsufficientBalance(),
+  );
 
   generateDocs(app);
   await app.listen(3000);
